4a7d0e22a8149f870c8cb8a22c2397827db7cd8d
Felipe Mamede <felipebrasilmv@gmail.com>
2023-09-20 23:00:26 -0300

    :wrench: add signin Header

--- src/screens/SignIn/SignIn.styles.ts
+++ src/screens/SignIn/SignIn.styles.ts
@@ -1,9 +1,37 @@
+import {SafeAreaView} from 'react-native-safe-area-context';
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
-  flex: 1;
+export const Container = styled(SafeAreaView)`
+  align-items: center;
+  justify-content: center;
 `;
 
-export const Text = styled.Text`
-  color: #f23ffa;
+export const HeaderContainer = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  height: 72px;
+  padding-left: 12px;
+  padding-right: 12px;
+`;
+
+export const HeaderContent = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+export const VoidContainer = styled.View`
+  width: 48px;
+`;
+
+export const BackContainer = styled.TouchableOpacity`
+  padding: 8px;
+`;
+
+export const TextHeader = styled.Text`
+  font-size: 20px;
+  margin-left: 12px;
 `;
--- src/screens/SignIn/SignIn.tsx
+++ src/screens/SignIn/SignIn.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 
+// ASSETS
+import Arrow from '@assets/icons/backArrow.svg';
+
 // STYLES
 import * as S from './SignIn.styles';
+import {useNavigation} from '@react-navigation/native';
 
 function SignIn() {
+  const navigation = useNavigation();
+
   return (
-    <S.Container>
-      <S.Text>Hello World SignIn</S.Text>
+    <S.Container edges={['top']}>
+      <S.HeaderContainer>
+        <S.HeaderContent>
+          <S.BackContainer onPress={(): void => navigation.goBack()}>
+            <Arrow width={16} height={16} color="#FFF" />
+          </S.BackContainer>
+          <S.TextHeader>Sign In</S.TextHeader>
+          <S.VoidContainer />
+        </S.HeaderContent>
+      </S.HeaderContainer>
     </S.Container>
   );
 }
