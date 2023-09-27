e653725b10d73c7be8b6e90f94d8973065008f62
Felipe Mamede <felipebrasilmv@gmail.com>
2023-09-26 22:21:34 -0300

    feat: add settings page with my infos

--- src/screens/Settings/Settings.styles.ts
+++ src/screens/Settings/Settings.styles.ts
@@ -2,8 +2,42 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const AvatarContainer = styled.View`
+  width: 49px;
+  height: 49px;
+  border-radius: 30px;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 16px;
+`;
+
+export const Avatar = styled.Image`
+  width: 60px;
+  height: 60px;
+  border-radius: 30px;
+`;
+
+export const Content = styled.View`
+  align-items: center;
+  justify-content: flex-start;
+  margin-bottom: 16px;
+  border-top-width: 1px;
+  border-top-color: #898989;
+  width: 100%;
 `;
 
 export const Text = styled.Text`
-  color: #f20;
+  color: #000;
+  font-size: 18px;
+  font-weight: 600;
+`;
+
+export const Linking = styled.TouchableOpacity`
+  color: #000;
+  font-size: 18px;
+  font-weight: 600;
 `;
--- src/screens/Settings/Settings.tsx
+++ src/screens/Settings/Settings.tsx
@@ -2,11 +2,39 @@ import React from 'react';
 
 // STYLES
 import * as S from './Settings.styles';
+import {Linking} from 'react-native';
 
 function Settings() {
   return (
     <S.Container>
-      <S.Text>Hello World Settings</S.Text>
+      <S.AvatarContainer>
+        <S.Avatar source={{uri: 'https://github.com/mamede.png'}} />
+      </S.AvatarContainer>
+      <S.Content>
+        <S.Text>Felipe Mamede</S.Text>
+        <S.Text>Desenvolvedor FrontEnd</S.Text>
+        <S.Text>Matrícula: 2373171025</S.Text>
+      </S.Content>
+
+      <S.Content>
+        <S.Text>Links úteis (clicaveis)</S.Text>
+        <S.Linking
+          onPress={(): void => {
+            Linking.canOpenURL('https://github.com/mamede').then(
+              (): Promise<any> => Linking.openURL('https://github.com/mamede'),
+            );
+          }}>
+          <S.Text>https://github.com/mamede</S.Text>
+        </S.Linking>
+        <S.Linking
+          onPress={(): void => {
+            Linking.canOpenURL('https://mamede.dev/').then(
+              (): Promise<any> => Linking.openURL('https://mamede.dev/'),
+            );
+          }}>
+          <S.Text>https://mamede.dev/</S.Text>
+        </S.Linking>
+      </S.Content>
     </S.Container>
   );
 }
